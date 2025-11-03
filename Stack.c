f7e2b9891c13f5ac7bac2be2f417c2fb9de35b74
Dhiraj2306 <dhirajpawar.a2306@gmail.com>
2025-11-03 21:05:58 +0530

    Update Stack.c

--- Stack.c
+++ Stack.c
@@ -1,3 +1,5 @@
+//1.
+
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 5
@@ -14,10 +16,7 @@ void push(STACK *s, int item)
     if (s->top == SIZE-1)
         printf("\nStack overflow");
     else
-    {
-        s->top+=1;
-        s->data[s->top]=item;
-    }
+        s->data[++(s->top)]=item;
 }
 
 void pop (STACK *s)
@@ -25,10 +24,7 @@ void pop (STACK *s)
     if (s->top==-1)
         printf("\nStack underflow");
     else
-    {
-        printf("\nelement poped is %d", s->data[s->top]);
-        s->top-=1;
-    }
+        printf("\nelement poped is %d", s->data[(s->top)--]);
 }
 
 void display(STACK s)
@@ -62,7 +58,7 @@ int main()
         switch(ch)
         {
             case 1:
-                printf("\nEnter the elementb to be pushed : ");
+                printf("\nEnter the element to be pushed : ");
                 scanf("%d",&item);
                 push(&s,item);
                 break;
