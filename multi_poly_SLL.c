42a5edcd9a145c3aa03a2d5438bf3dd099439b24
Dhiraj2306 <dhirajpawar.a2306@gmail.com>
2025-11-13 15:19:44 +0530

    Update multi_poly_SLL.c

--- multi_poly_SLL.c
+++ multi_poly_SLL.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 struct node
 {
     int co, po;
     struct node *addr;
 };
 typedef struct node * NODE;
-
 
 NODE insertend (NODE start, int co, int po)
 {
@@ -28,7 +26,6 @@ NODE insertend (NODE start, int co, int po)
     return start;
 }
 
-
 void display(NODE start)
 {
     NODE temp;
@@ -45,7 +42,6 @@ void display(NODE start)
         printf("%d*x^%d", temp->co, temp->po);
     }
 }
-
 
 NODE addterm(NODE res, int co, int po)
 {
@@ -74,55 +70,45 @@ NODE addterm(NODE res, int co, int po)
     }
 }
 
-
 NODE multiply(NODE poly1, NODE poly2)
 {
-    NODE p1,p2,res=NULL;
-
-    for(p1=poly1 ; p1!=NULL ; p1=p1->addr)
-        for(p2=poly2 ; p2!=NULL ; p2=p2->addr)
-            res=addterm(res, p1->co*p2->co, p1->po+p2->po);
-    
+    NODE p1, p2, res=NULL;
+    for (p1=poly1; p1!=NULL; p1=p1->addr)
+            for (p2=poly2; p2!=NULL; p2=p2->addr)
+                res=addterm(res, p1->co*p2->co, p1->po+p2->po);
     return res;
 }
-
 
 int main()
 {
     NODE poly1=NULL, poly2=NULL, poly;
     int n, i, co, po;
-    
     printf("\nRead number of terms of 1st polynomial : ");
     scanf("%d", &n);
 
-    for(i=1 ; i<=n ; i++)
+    for (i=1; i<=n ; i++)
     {
-        printf("\nRead co and po of %d term : ", i);
+        printf("\nRead co and po of %d term", i);
         scanf("%d %d", &co, &po);
-
         poly1=insertend(poly1, co, po);
     }
     printf("\nFirst polynomial is \n");
     display(poly1);
 
-
-    printf("\nRead number of terms of 2st polynomial : ");
+    printf("\nRead number of terms of 2nd polynomial : ");
     scanf("%d", &n);
 
-    for(i=1 ; i<=n ; i++)
+    for (i=1; i<=n ; i++)
     {
-        printf("\nRead co and po of %d term : ", i);
+        printf("\nRead co and po of %d term", i);
         scanf("%d %d", &co, &po);
-
         poly2=insertend(poly2, co, po);
     }
-    printf("\nFirst polynomial is \n");
+    printf("\nSecond polynomial is \n");
     display(poly2);
 
     poly=multiply(poly1, poly2);
-
     printf("\nResultant polynomial is \n");
     display(poly);
-    
     return 0;
 }
